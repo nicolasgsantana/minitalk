bc2ffd2546afbdd03717923bf21f1e9128f04ce0
nicolasgsantana <nicolas07san@gmail.com>
2025-09-29 15:51:56 -0300

    add receiving bits logic

--- src/server.c
+++ src/server.c
@@ -6,18 +6,34 @@
 /*   By: nde-sant <nde-sant@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/29 11:59:35 by nde-sant          #+#    #+#             */
-/*   Updated: 2025/09/29 13:20:18 by nde-sant         ###   ########.fr       */
+/*   Updated: 2025/09/29 15:50:57 by nde-sant         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
+void	print_byte(char bit)
+{
+	static int	bit_count = 0;
+	static char	current_char = 0;
+
+	if (bit_count < 8)
+		current_char = (current_char ^ bit) << 1;
+	bit_count++;
+	if (bit_count >= 8)
+	{
+		ft_printf("%c", current_char);
+		bit_count = 0;
+		current_char = 0;
+	}
+}
+
 void	signal_handler(int signum)
 {
 	if (signum == SIGUSR1)
-		ft_printf("Signal SIGUSR1 received\n");
+		print_byte(0);
 	else if (signum == SIGUSR2)
-		ft_printf("Signal SIGUSR2 received\n");
+		print_byte(1);
 }
 
 int	main(void)
@@ -27,6 +43,8 @@ int	main(void)
 	signal(SIGUSR1, signal_handler);
 	signal(SIGUSR2, signal_handler);
 	while (1)
+	{
 		pause();
+	}
 	return (0);
 }
