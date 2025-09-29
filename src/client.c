57de937aa3452517abf33b7d8cfe6732024ae02c
nicolasgsantana <nicolas07san@gmail.com>
2025-09-29 15:11:33 -0300

    add bit sending logic

--- src/client.c
+++ src/client.c
@@ -6,11 +6,35 @@
 /*   By: nde-sant <nde-sant@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/29 12:00:40 by nde-sant          #+#    #+#             */
-/*   Updated: 2025/09/29 13:19:39 by nde-sant         ###   ########.fr       */
+/*   Updated: 2025/09/29 15:05:26 by nde-sant         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
+
+void	send_bits(char *message, int pid)
+{
+	int				i;
+	char			mask;
+	unsigned char	bit;
+
+	while (*message)
+	{
+		i = 7;
+		while (i >= 0)
+		{
+			mask = 1 << i;
+			bit = (unsigned char)((*message & mask) >> i);
+			if (bit == 0)
+				kill(pid, SIGUSR1);
+			if (bit == 1)
+				kill(pid, SIGUSR2);
+			i--;
+			usleep(100);
+		}
+		message++;
+	}
+}
 
 int	main(int argc, char **argv)
 {
@@ -21,10 +45,7 @@ int	main(int argc, char **argv)
 		pid = ft_atoi(argv[1]);
 		message = argv[2];
 		ft_printf("Message '%s' sent to pid %d\n", message, pid);
-		if (message[0] == '1')
-			kill(pid, SIGUSR1);
-		else if (message[0] == '2')
-			kill(pid, SIGUSR2);
+		send_bits(message, pid);
 	}
 	return (0);
 }
