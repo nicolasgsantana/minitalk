b685b159cbadfcdf85ad9daced2d3907b51a8900
nicolasgsantana <nicolas07san@gmail.com>
2025-09-29 13:23:14 -0300

    add first tests with signal comunication

--- src/client.c
+++ src/client.c
@@ -6,7 +6,7 @@
 /*   By: nde-sant <nde-sant@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/29 12:00:40 by nde-sant          #+#    #+#             */
-/*   Updated: 2025/09/29 12:11:01 by nde-sant         ###   ########.fr       */
+/*   Updated: 2025/09/29 13:19:39 by nde-sant         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,17 @@
 
 int	main(int argc, char **argv)
 {
+	int		pid;
+	char	*message;
 	if (argc == 3)
-		ft_printf("PID: %s, Message to be sent: %s\n", argv[1], argv[2]);
+	{
+		pid = ft_atoi(argv[1]);
+		message = argv[2];
+		ft_printf("Message '%s' sent to pid %d\n", message, pid);
+		if (message[0] == '1')
+			kill(pid, SIGUSR1);
+		else if (message[0] == '2')
+			kill(pid, SIGUSR2);
+	}
 	return (0);
 }
--- src/server.c
+++ src/server.c
@@ -6,14 +6,27 @@
 /*   By: nde-sant <nde-sant@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/29 11:59:35 by nde-sant          #+#    #+#             */
-/*   Updated: 2025/09/29 12:10:56 by nde-sant         ###   ########.fr       */
+/*   Updated: 2025/09/29 13:20:18 by nde-sant         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
+void	signal_handler(int signum)
+{
+	if (signum == SIGUSR1)
+		ft_printf("Signal SIGUSR1 received\n");
+	else if (signum == SIGUSR2)
+		ft_printf("Signal SIGUSR2 received\n");
+}
+
 int	main(void)
 {
 	ft_printf("Server started\n");
+	ft_printf("PID: %d\n", getpid());
+	signal(SIGUSR1, signal_handler);
+	signal(SIGUSR2, signal_handler);
+	while (1)
+		pause();
 	return (0);
 }
