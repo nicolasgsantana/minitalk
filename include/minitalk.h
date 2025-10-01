c5c38e09af343211ffc4dc56329082eeac06cf13
nicolasgsantana <nicolas07san@gmail.com>
2025-10-01 11:05:40 -0300

    add bit_delay

--- include/minitalk.h
+++ include/minitalk.h
@@ -6,9 +6,16 @@
 /*   By: nde-sant <nde-sant@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/29 12:00:34 by nde-sant          #+#    #+#             */
-/*   Updated: 2025/09/29 12:00:35 by nde-sant         ###   ########.fr       */
+/*   Updated: 2025/10/01 10:52:44 by nde-sant         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
-#include <signal.h>
+#ifndef MINITALK_H
+# define MINITALK_H
+
+# include "libft.h"
+# include <signal.h>
+
+# define BIT_DELAY 500
+
+#endif
