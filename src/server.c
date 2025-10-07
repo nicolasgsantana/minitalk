1600e5254853ed57cff176c014d84674099a8595
nicolasgsantana <nicolas07san@gmail.com>
2025-10-07 18:12:31 -0300

    fix logic errors

--- src/server.c
+++ src/server.c
@@ -6,7 +6,7 @@
 /*   By: nde-sant <nde-sant@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/29 11:59:35 by nde-sant          #+#    #+#             */
-/*   Updated: 2025/10/07 16:33:06 by nde-sant         ###   ########.fr       */
+/*   Updated: 2025/10/07 18:07:06 by nde-sant         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,31 +25,32 @@ void	write_byte(char bit)
 {
 	g_server.byte = (g_server.byte << 1) | bit;
 	g_server.bit_count++;
-	kill(g_server.client_pid, SIGUSR1);
 	if (g_server.bit_count == 8)
 	{
 		if (!g_server.byte)
 		{
 			write(STDOUT_FILENO, "\n", 1);
 			kill(g_server.client_pid, SIGUSR2);
-			g_server.client_pid = 0;
+			init_server();
+			return ;
 		}
 		else
 			write(STDOUT_FILENO, &g_server.byte, 1);
 		g_server.bit_count = 0;
 		g_server.byte = 0;
 	}
+	kill(g_server.client_pid, SIGUSR1);
 }
 
 void	signal_handler(int signum, siginfo_t *info, void *context)
 {
 	(void)context;
-	if (g_server.client_pid != 0)
+	if (g_server.client_pid == 0)
 		g_server.client_pid = info->si_pid;
 	if (signum == SIGUSR1)
 		write_byte(0);
 	if (signum == SIGUSR2)
-		write_byte(0);
+		write_byte(1);
 }
 
 int	main(void)
