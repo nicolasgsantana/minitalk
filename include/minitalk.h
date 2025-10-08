834de81da30d8a597b1dc0b32c5cc802c6f465d8
nicolasgsantana <nicolas07san@gmail.com>
2025-10-08 10:58:47 -0300

    update messages

--- include/minitalk.h
+++ include/minitalk.h
@@ -6,7 +6,7 @@
 /*   By: nde-sant <nde-sant@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/29 12:00:34 by nde-sant          #+#    #+#             */
-/*   Updated: 2025/10/07 18:14:30 by nde-sant         ###   ########.fr       */
+/*   Updated: 2025/10/08 10:31:52 by nde-sant         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,8 @@
 # include <signal.h>
 
 # define INPUT_ERR "Invalid Input.\nUsage:\n\t./client [server_pid] [message]\n"
-# define PID_ERR "Operation not permitted with current PID.\n"
+# define PID_ERR "Operation not allowed with current PID.\n"
+# define SERVER_CONFIRM "Server confirmation received.\n"
 
 typedef struct	s_client
 {
